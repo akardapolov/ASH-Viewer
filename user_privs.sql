db2d44169f0b110a85d96d153162c61d80719747
akardapolov <akardapolov@gmail.com>
2014-11-24 17:29:34 +0500

    user_privs.sql
    Create monitor user and grant select and execute priviliges to connect to Oracle DB from ASH Viewer unility.

---
+++ user_privs.sql
@@ -0,0 +1,26 @@
+-- Create monitor user and grant select and execute priviliges to connect to Oracle DB (ASH Viewer)
+create user monitor identified by monitor;
+
+grant connect to monitor;
+
+grant select on dba_users to monitor;
+grant select on v_$sql to monitor;
+grant select on v_$sql_plan to monitor;
+grant select on v_$active_session_history to monitor;
+grant select on v_$parameter to monitor;
+grant select on v_$database to monitor;
+grant select on v_$instance to monitor;
+
+
+grant execute on DBMS_APPLICATION_INFO to monitor;
+grant execute on DBMS_WORKLOAD_REPOSITORY to monitor;
+grant execute on DBMS_XPLAN to monitor;
+
+grant select_catalog_role to monitor;
+
+grant select on DBA_HIST_BASELINE monitor;
+grant select on DBA_HIST_SQLTEXT to monitor;
+grant select on DBA_HIST_SQLSTAT to monitor;
+grant select on DBA_HIST_SQLBIND to monitor;
+grant select on DBA_HIST_OPTIMIZER_ENV to monitor;
+grant select on DBA_HIST_SNAPSHOT to monitor;
